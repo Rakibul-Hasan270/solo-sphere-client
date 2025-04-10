c355fd7389d8afba80d67461853429297d3be14d
Rakibul-Hasan270 <rakibx270@gmail.com>
2025-04-10 11:42:22 +0600

    added browser router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import AuthProvider from './provider/AuthProvider'
+import { RouterProvider } from 'react-router'
+import router from './routes/Router'
 // import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
+    <AuthProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </AuthProvider>
   </StrictMode>,
 )
